06db256e439977e8fd014c45f10f4dd98b6877c5
Dmitry Kornilov <dmitry.kornilov@oracle.com>
2017-03-06 16:47:59 +0100

    Javadoc fixes.
    Signed-off-by: Dmitry Kornilov <dmitry.kornilov@oracle.com>

--- api/src/main/java/javax/json/bind/config/BinaryDataStrategy.java
+++ api/src/main/java/javax/json/bind/config/BinaryDataStrategy.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2015, 2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -41,10 +41,8 @@
 package javax.json.bind.config;
 
 /**
- * <p>Allows to define custom binary data handing strategy.
- * Specifies predefined binary data handling strategies.</p>
- *
- * <p>This strategy can be set via {@link javax.json.bind.JsonbConfig}.</p>
+ * Specifies predefined binary data handling strategies.
+ * This strategy can be set via {@link javax.json.bind.JsonbConfig#withBinaryDataStrategy(String)}.
  *
  * @see javax.json.bind.JsonbConfig
  * @since JSON Binding 1.0
